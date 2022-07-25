f7f49a86338de5e49b83a902eb7df6ae08ace8de
Ali M <alimousavi-01@outlook.com>
2022-07-25 19:42:13 +0430

    Added WeatherSettings

--- dotnet-weather-service/API/Services/HourlyWeatherBackgroundService.cs
+++ dotnet-weather-service/API/Services/HourlyWeatherBackgroundService.cs
@@ -2,9 +2,12 @@
 
 public class HourlyWeatherBackgroundService : BackgroundService
 {
+    private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(5));
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        while (await _timer.WaitForNextTickAsync(stoppingToken)
+                && !stoppingToken.IsCancellationRequested)
         {
             Console.WriteLine("BackgroundService is working fine !");
             await Task.Delay(1000, stoppingToken);
---
+++ dotnet-weather-service/Service/Infrastructure/WeatherSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Service.Infrastructure;
+
+public class WeatherSettings
+{
+    /// <summary>
+    /// Key of application openweathermap
+    /// </summary>
+    public string ApiKey { get; set; }
+
+    /// <summary>
+    /// Base url of api openweathermap
+    /// </summary>
+    public string BaseUrl { get; set; }
+
+    /// <summary>
+    /// Version of API
+    /// </summary>
+    public string ApiVersion { get; set; }
+}
