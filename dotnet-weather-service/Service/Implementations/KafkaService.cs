21096aecd75510110b9745d6e823b69a709c8fc3
Ali M <alimousavi-01@outlook.com>
2022-08-05 22:36:11 +0430

    added polly to kafkaservice

--- dotnet-weather-service/Service/Implementations/KafkaService.cs
+++ dotnet-weather-service/Service/Implementations/KafkaService.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Service.Infrastructure;
 using Service.Models;
+using Polly;
+using Polly.Retry;
 
 namespace Service.Implementations;
 
@@ -10,6 +12,9 @@ public class KafkaService : Intefaces.IKafkaService
 {
     private readonly ILogger<KafkaService> _logger;
     private readonly IProducer<string, string> _producer;
+
+    private const int MAX_RETRIES = 3;
+    private readonly AsyncRetryPolicy _retry;
 
     public KafkaService(ILogger<KafkaService> logger, IOptions<KafkaSettings> kafkaSettings)
     {
@@ -21,25 +26,30 @@ public class KafkaService : Intefaces.IKafkaService
         };
 
         _producer = new ProducerBuilder<string, string>(config).Build();
+
+        _retry = Policy.Handle<KafkaException>()
+                       .WaitAndRetryAsync(
+                            retryCount: MAX_RETRIES,
+                            sleepDurationProvider: times => TimeSpan.FromSeconds(times),
+                            onRetry: (err, sleepDuration, attemptNumber, context) =>
+                            {
+                                _logger.LogError("ERROR : {message}. Retrying in {sleepDuration}. {attemptNumber} / {max_retries}", err.Message, sleepDuration, attemptNumber, MAX_RETRIES);
+                            });
     }
 
     public async Task ProduceAsync(string city, Weather weather)
     {
-        try
+        await _retry.ExecuteAsync(async () =>
         {
             var message = new Message<string, string>
             {
                 Key = Guid.NewGuid().ToString(),
                 Value = string.Format("The temperature in {0}, {1} is currently {2} °C", city, weather.SystemWeather.Country, weather.Main.CelsiusCurrent)
             };
-            
+
             await _producer.ProduceAsync("weather_app", message);
-            
+
             _logger.LogInformation("The temperature in {city}, {country} is currently {temperature} °C", city, weather.SystemWeather.Country, weather.Main.CelsiusCurrent);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError("ERROR : {message}", ex.Message);
-        }
+        });
     }
 }
