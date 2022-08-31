308fe0a7053f5ee509693c38869e8aef64c77264
Ali M <ali.mo.dev@gmail.com>
2022-08-31 16:44:48 +0430

    Unit Tests finished

---
+++ dotnet-weather-producer/tests/UnitTests/Helpers/MockHttpMessageHandler.cs
@@ -0,0 +1,50 @@
+﻿using Moq.Protected;
+using Moq;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace UnitTests.Helpers;
+
+internal class MockHttpMessageHandler
+{
+    internal Mock<HttpMessageHandler> SetupResponse<T>(T expectedResponse)
+    {
+        var mockResponse = new HttpResponseMessage
+        {
+            Content = new StringContent(JsonConvert.SerializeObject(expectedResponse)),
+            StatusCode = HttpStatusCode.OK
+        };
+
+        mockResponse.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        var handlerMock = new Mock<HttpMessageHandler>();
+
+        handlerMock.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                                 ItExpr.IsAny<HttpRequestMessage>(),
+                                                                 ItExpr.IsAny<CancellationToken>())
+                                .ReturnsAsync(mockResponse);
+
+        return handlerMock;
+    }
+
+    internal Mock<HttpMessageHandler> SetupReturnNotFound()
+    {
+        var mockResponse = new HttpResponseMessage
+        {
+            Content = new StringContent(""),
+            StatusCode = HttpStatusCode.NotFound
+        };
+
+        mockResponse.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        var handlerMock = new Mock<HttpMessageHandler>();
+
+        handlerMock.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+                                                                 ItExpr.IsAny<HttpRequestMessage>(),
+                                                                 ItExpr.IsAny<CancellationToken>())
+                                .ReturnsAsync(mockResponse);
+
+        return handlerMock;
+    }
+}
--- dotnet-weather-producer/tests/UnitTests/Services/WeatherClientTests.cs
+++ dotnet-weather-producer/tests/UnitTests/Services/WeatherClientTests.cs
@@ -3,31 +3,99 @@ using Core.Settings;
 using FluentAssertions;
 using Infrastructure.Clients;
 using Microsoft.Extensions.Options;
+using Moq.Protected;
+using Moq;
+using UnitTests.Helpers;
 
 namespace UnitTests.Services;
 public class WeatherClientTests
 {
-    private readonly WeatherClient _sut;
+    private readonly IOptions<WeatherSettings> _config;
+    private readonly MockHttpMessageHandler _handlerMock;
     public WeatherClientTests()
     {
-        var config = Options.Create(new WeatherSettings());
-        _sut = new WeatherClient(config, new HttpClient());
+        _config = Options.Create(new WeatherSettings());
+        _handlerMock = new MockHttpMessageHandler();
     }
 
     [Fact]
     public async Task GetWeatherAsync_WhenCityIsNull_ThrowsArgumentNullException()
     {
-        var result = async () => await _sut.GetWeatherAsync(null);
+        // Arrange
+        var handlerMock = _handlerMock.SetupResponse(new Weather());
+        var client = new HttpClient(handlerMock.Object);
 
+        var sut = new WeatherClient(_config, client);
+
+        // Act
+        var result = async () => await sut.GetWeatherAsync(null);
+
+        // Assert
         await result.Should().ThrowAsync<ArgumentNullException>();
     }
 
     [Fact]
     public async Task GetWeatherAsync_WhenCityIsEmpty_ThrowsArgumentNullException()
     {
-        var result = async () => await _sut.GetWeatherAsync(string.Empty);
+        // Arrange
+        var handlerMock = _handlerMock.SetupResponse(new Weather());
+        var client = new HttpClient(handlerMock.Object);
 
+        var sut = new WeatherClient(_config, client);
+
+        // Act
+        var result = async () => await sut.GetWeatherAsync(string.Empty);
+
+        // Assert
         await result.Should().ThrowAsync<ArgumentNullException>();
+    }
+
+    [Fact]
+    public async Task GetWeatherAsync_InvalidCity_ReturnsNotFound()
+    {
+        // Arrange
+        var handlerMock = _handlerMock.SetupReturnNotFound();
+        var client = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("http://example.com")
+        };
+
+        var sut = new WeatherClient(_config, client);
+
+        // Act
+        var result = async () => await sut.GetWeatherAsync("Invalid City");
+
+        // Assert
+        await result.Should().ThrowAsync<HttpRequestException>();
+        handlerMock.Protected()
+                    .Verify("SendAsync",
+                            Times.Exactly(1),
+                            ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
+                            ItExpr.IsAny<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task GetWeatherAsync_InvalidCity_ReturnsResponse()
+    {
+        // Arrange
+        var handlerMock = _handlerMock.SetupResponse(new Weather());
+        var client = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("http://example.com")
+        };
+
+        var sut = new WeatherClient(_config, client);
+
+        // Act
+        var result = await sut.GetWeatherAsync("chicago");
+
+        // Assert
+        handlerMock.Protected()
+                    .Verify("SendAsync",
+                            Times.Exactly(1),
+                            ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
+                            ItExpr.IsAny<CancellationToken>());
+        result.Should().BeOfType<Weather>();
     }
 
 
